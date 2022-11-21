59a1bae5865f8fe6d2fc66020bf09ca3449e124c
elisagdelope <elisa.gdl95@gmail.com>
2022-11-21 13:57:53 +0000

    add plot & ft.imp. functions compatible w/ RBFSVM model

--- digipd_ml/plot_utils.py
+++ digipd_ml/plot_utils.py
@@ -45,3 +45,45 @@ def plot_shap_values(model, X, save_fig=False, name_file=None, path=None, names_
         fig.savefig(path + name_file)
  #   fig.show()
     plt.close(fig)
+
+
+
+def plot_from_shap_values(shap_values, X, save_fig=False, name_file=None, path=None, names_list=None, plot_title=None):
+    """Plot shap values for a given scikit-learn model.
+
+    Parameters
+    ----------
+    shap_values : array, shape (n_samples, n_features)
+                  Shap values matrix
+    X : array, shape (n_samples, n_features).
+        Training data.
+    save_fig : bool
+               Whether or not to save the figures.
+    name_file : str
+                Name of the file if saved.
+    path : str
+           Path for the saved figure.
+    names_list : list
+                 Names of the features (length # features) to appear in the plot.
+    plot_title : str
+                 Title of the plot.
+    """
+
+    if not names_list:
+        names_list = list(X.columns)
+
+    shap_fig = shap.summary_plot(shap_values=shap_values, features=X, feature_names=names_list, show=False)
+#    shap.plots.beeswarm(shap_values, show=False)
+    fig, ax = plt.gcf(), plt.gca()
+    labels = ax.get_yticklabels()
+    fig.axes[-1].set_aspect(150)  # set the color bar
+    fig.axes[-1].set_box_aspect(150)  # set the color bar
+    fig.set_size_inches(15, 10)  # set figure size
+    ax.set_yticklabels(labels, fontsize=12)
+    ax.set_xticklabels(np.round(ax.get_xticks(), 2), rotation=15, fontsize=8)
+    plt.title(plot_title)
+    fig.tight_layout()
+    if save_fig:
+        fig.savefig(path + name_file)
+ #   fig.show()
+    plt.close(fig)
--- digipd_ml/utils.py
+++ digipd_ml/utils.py
@@ -67,3 +67,70 @@ def feature_importances_shap_values(model, X, names_list=None, n=20):
     shap_importance = shap_importance.iloc[0:n,]
     
     return shap_importance
+
+
+def feature_importances_shap_values(model, X, names_list=None, n=20):
+    """
+    Extracts the top n relevant features based on SHAP values in an ordered way
+
+    Parameters
+    ----------
+    model : instance of BaseEstimator from scikit-learn.
+            Model for which shap values have to be computed.
+    X : array, shape (n_samples, n_features).
+        Training data.
+    names_list : list
+                 Names of the features (length # features) to appear in the plot.
+    n : number of features to retrieve
+    """
+
+    # generate shap values
+    explainer = shap.Explainer(model, X)
+    shap_values = explainer(X)
+    if np.ndim(shap_values) == 3:
+        shap_values = shap_values[:, :, 1]
+    if not names_list:
+        names_list = list(X.columns)
+    shap_df = pd.DataFrame(shap_values.values, columns=names_list)
+    vals = np.abs(shap_df).mean(0)
+    shap_importance = pd.DataFrame(list(zip(names_list, vals)),
+                                   columns=['feature', 'shap_value'])
+    shap_importance.sort_values(by=['shap_value'],
+                                ascending=False,
+                                inplace=True)
+    shap_importance = shap_importance.iloc[0:n, ]
+
+    return shap_importance
+
+
+
+
+def feature_importances_from_shap_values(shap_values, X, names_list=None, n=20):
+    """
+    Extracts the top n relevant features in an ordered way, given a matrix of shap values
+
+    Parameters
+    ----------
+    shap_values : array, shape (n_samples, n_features)
+                  Shap values matrix
+    X : array, shape (n_samples, n_features).
+        Training data.
+    names_list : list
+                 Names of the features (length # features) to appear in the plot.
+    n : number of features to retrieve
+    """
+
+    if not names_list:
+        names_list = list(X.columns)
+
+    shap_df = pd.DataFrame(shap_values, columns=names_list)
+    vals = np.abs(shap_df).mean(0)
+    shap_importance = pd.DataFrame(list(zip(names_list, vals)),
+                                   columns=['feature', 'shap_value'])
+    shap_importance.sort_values(by=['shap_value'],
+                                ascending=False,
+                                inplace=True)
+    shap_importance = shap_importance.iloc[0:n, ]
+
+    return shap_importance
+
